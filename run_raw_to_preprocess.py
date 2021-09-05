224b66d8aefcfd16754a155e7715cf7e9d05c727
nail1021734 <nail1021734@gmail.com>
2021-09-05 14:17:11 +0800

    Remove useless file.

--- run_raw_to_preprocess.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import news.db
-import gc
-if __name__ == "__main__":
-    data = [news.db.read.AllRecords(db_name='ftv.db') for _ in range(20)]
-    # urls = set([n.url for n in data])
-    # del data
-    # gc.collect()
-    input()
